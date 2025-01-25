a226d330fec18a369f88f956ec4634d4ff59a335
razrahul <razrahuleee.com>
2025-01-25 19:44:43 +0530

    project setup Completed

--- src/app.js
+++ src/app.js
@@ -1,34 +1,29 @@
 import express from "express";
-import {config} from "dotenv"
+import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
-
 config({
     path: "./.env",
-  });
-
+});
 
 const app = express();
 
 const corsOptions = {
-  origin: [process.env.LOCALHOST_URL, process.env.FRONTEND_URL], // Allows all origins
-  methods: ["GET","POST","PUT","DELETE"],
+  origin: [process.env.LOCALHOST_URL, process.env.FRONTEND_URL], // Allows specified origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Allows cookies and other credentials to be sent with the request
 };
 
+app.use(cors(corsOptions)); // Enable CORS for specified origins
 
-
-app.use(cors(corsOptions)); // Enable CORS for all origins
-
-//using middlewares
+// Using middlewares
 app.use(express.json());
 app.use(
   express.urlencoded({
       extended: true,
   })
-)
+);
 
 app.use(cookieParser());
 
@@ -39,13 +34,8 @@ app.get("/test", (req, res) => {
   });
 });
 
-
-app.get("/cheack", (req, res) => {
-  res.send("Nice working");
+app.get("/check", (req, res) => {
+  res.send("Nice working cheack and complete");
 });
-
-
-
-
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import app from "./app.js";
+import { config } from 'dotenv';
 
+config();
 
-
-
-const Port = process.env.PORT;
+const Port = process.env.PORT || 5000;  // Ensure a default port is set
 
 app.listen(Port, () => {
-    console.log(`Server is running on port ${Port}`)
+    console.log(`Server is running on port ${Port}`);
     // logger.info("⚙️  Server is running on port: " + process.env.PORT);
-})
+});
 
 
 
